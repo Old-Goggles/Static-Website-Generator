9001d0d1c8d75f2a62bdf50727b9a37ef7c97835
Nathan Conlon <nconlon@pm.me>
2025-06-23 16:51:03 -0500

    feat: add recursive static directory copy and clean public output

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
-from textnode import TextNode
-from textnode import TextType
+import os
+import shutil
+
+def copy_static_recursive(src, dst):
+    os.makedirs(dst, exist_ok=True)
+    items = os.listdir(src)
+    for item in items:
+        full_path = os.path.join(src, item)
+        if os.path.isfile(full_path):
+            src_full_path = os.path.join(src, item)
+            dst_full_path = os.path.join(dst, item)
+            shutil.copy(src_full_path, dst_full_path)
+
+        elif os.path.isdir(full_path):
+                src_subdir = os.path.join(src, item)
+                dst_subdir = os.path.join(dst, item)
+                copy_static_recursive(src_subdir, dst_subdir)
 
 def main():
-    node = TextNode("bs text", TextType.BOLD,)
-    print(node)
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    copy_static_recursive("static", "public")
 
-main()
+if __name__ == "__main__":
+    main()
